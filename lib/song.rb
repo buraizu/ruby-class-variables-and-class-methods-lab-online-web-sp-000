cf21157f1930b101aa58fdb63a9a9e8637a6a5d1
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-10 07:46:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,5 +27,16 @@ class Song
     @@genres.uniq
   end
 
+  def self.genre_count
+    return_hash = {}
+    @@genres.each do |element|
+      if return_hash[element] == nil 
+        return_hash[element] = 1
+      else
+        return_hash[element] += 1
+      end
+    end
+  end
+
 
 end
